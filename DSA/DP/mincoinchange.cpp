7cccaacd8ac0005b7f027cc182e84109275810de
Pratham08 <prathampali0909@gmail.com>
2023-01-14 11:40:56 +0530

    commit

--- DSA/DP/mincoinchange.cpp
+++ DSA/DP/mincoinchange.cpp
@@ -18,33 +18,23 @@ int minCoins(int coins[], int M, int V)
     //     }
     // }
     // return dp[V - 1] = ans;
-
     if (V == 1)
-    {
         return 1;
-    }
     if (dp[V] != INT_MAX)
         return dp[V];
     int ans = INT_MAX;
     for (int i = 0; i < M; i++)
-    {
-
         if (V - coins[i] >= 0)
-            ans =  min(ans, 1 + minCoins(coins, M, V - coins[i]));
-    }
-    dp[V-1] = ans;
-    return dp[V-1];
+            ans = min(ans, 1 + minCoins(coins, M, V - coins[i]));
+    dp[V - 1] = ans;
+    return dp[V - 1];
 }
 
 int main()
 {
-
     int coins[8] = {9, 2, 11, 5, 14, 17, 8, 18};
     cout << minCoins(coins, 8, 39);
     for (int i = 1; i < 40; i++)
-    {
         cout << dp[i] << "\n";
-    }
-
     return 0;
 }
